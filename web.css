64f9458c1c7384ab846f0fd8d15d64a14914ab09
Premjaina <119107227+Premjaina@users.noreply.github.com>
2022-12-02 21:58:31 +0530

    Add files via upload

---
+++ web.css
@@ -0,0 +1,284 @@
+.main{
+    font-weight: bold;
+    font-style: italic;
+    bottom: 2px;
+}
+*{
+    margin: 0;
+    padding:0;
+}
+.main{
+    width: 100%;z
+    background: linear-gradient(to top, rgba(2, 2, 2, 0.5)50%,rgba(0,0,0,0.5)50%), url(1.jpg);
+    background-position: center;
+    background-size: cover;
+    height:60px
+}
+body{
+background-image: url("https://tse4.mm.bing.net/th?id=OIP.HgqymfnU4zHIvwRg9iGPnQHaFj&pid=Api&P=0");
+background-repeat:no-repeat;
+background-size:contain;
+background-position: center;
+background-attachment: fixed;
+background-size: cover;
+
+}
+.navbar{
+    width: 1200px;
+    height:  75px;
+    margin: auto;
+}
+.icon{
+    width: 30px;
+    float: left;
+    height: 70px;
+}
+.menu{
+    width: 400px;
+    float: left;
+    height: 50px;
+}
+.logo{
+    padding-left: -10px;
+}
+ul{
+    float: left;
+    display: flex;
+    justify-content: right;
+    align-items: right;
+}
+ul li{
+    list-style: none;
+    margin-left: 160px;
+    margin-top: 20px;
+    font-size: 13px;
+    list-style-position: left;
+    padding-left: 30px;
+}
+ul li a{
+    text-decoration: none;
+    color: white;
+    font-family: Arial;
+    font-weight: bold;
+    transition: 0.4s ease-in-out;
+    text-align: left;
+    gap: 2px;
+}
+ul li a:hover{
+    color: orange;
+}
+.search{
+    width: 330px;
+    float: right;
+    margin-right: 870px;
+}
+.srch{
+    font-family: 'Times New Roman', Times, serif;
+    width: 200px;
+    height: 40px;
+    background: transparent;
+    
+    margin-top: 1000px;
+    color: black;
+    border-right:4px;
+    float: left;
+    padding: 10px;
+    border-bottom-right-radius: 5px;
+    border-top-right-radius: 5px;
+}
+.btn{
+    width: 1000px;
+    height: 39px;
+    background: orange;
+    border: 2px solid #ff7200;
+    margin-top: 50px;
+    color: #fff;
+    border-bottom-left-radius: 5px;
+    border-top-left-radius: 5px;
+}
+.btn:focus{
+    outline: none;
+}
+.note{
+    font-style: arial;
+    font-weight: bold;
+    margin-top: 15px;
+    margin-right: 600px;
+    
+}
+.imp{
+    font-size: 14px;
+    color: blue;
+}
+hr{
+    border:none;
+    height: 3px;
+    background-color: black;
+}
+.form{
+    width: 250px;
+    color: aliceblue;
+    height: 380px;
+    background: linear-gradient(to top,rgba(0,0,0,0.8)50%,rgba(0,0,0,0.8)50%);
+    position: absolute;
+    top: 300px;
+    right: 1050px;
+    border-radius: 10px;
+    padding: 25px;
+}
+.form h2{
+    width: 220px;
+    font-family: sans-serif;
+    text-align: center;
+    color: orange;
+    font-size: 20px;
+    background-color: white;
+    border-radius: 10px;
+    margin: 2px;
+    padding: 8px;
+}
+.form input{
+    width: 240px;
+    height: 35px;
+    background: transparent;
+    border-bottom: 1px solid orange;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    color: white;
+    font-size: 15px;
+    letter-spacing: 1px;
+    margin-top: 30px;
+    font-family: sans-serif;
+}
+.form input:focus{
+    outline:none;
+}
+::placeholder{
+    color: white;
+    font-family: Arial;
+}
+.btnn {
+    width: 240px;
+    height: 40px;
+    background: orange;
+    border: none;
+    margin-top: 30px;
+    font-size: 18px;
+    border-radius: 10px;
+    cursor: pointer;
+    color: antiquewhite;
+}
+.btnn:hover{
+    background: white;
+    color: orange;
+}
+.btnn a{
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+}
+.form .linkd{
+    font-family: arial;
+    font-size: 17px;
+    padding-top: 20px;
+    text-align: center;
+}
+.form .linkd a{
+    text-decoration: none;
+    color: orange;
+}
+.liw{
+    padding-top: 15px;
+    padding-bottom: 10px;
+    text-align: center;
+}
+.non{
+    width: 400px;
+    height: 380px;
+    background: linear-gradient(to top,rgba(0,0,0,0.8)50%,rgba(0,0,0,0.8)50%);
+    position: absolute;
+    top: 300px;
+    left: 500px;
+    border-radius: 10px;
+    padding: 25px;
+}
+.non h3{
+   
+    font-family: sans-serif;
+    text-align: center;
+    color: rgb(204, 128, 29);
+    font-size: 30px;
+   margin-bottom: 30px;
+}
+
+ .non .nn{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+}
+.non .ll{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+    margin-top: 30px;   
+}
+.non .jj{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+    margin-top: 30px;   
+}
+.non .cc{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+    margin-top: 30px;  
+}
+.no{
+    width: 330px;
+    height: 380px;
+    background: linear-gradient(to top,rgba(0,0,0,0.8)50%,rgba(0,0,0,0.8)50%);
+    position: absolute;
+    top: 300px;
+    left: 990px;
+    border-radius: 10px;
+    padding: 25px;
+}
+.no h3{
+   
+    font-family: sans-serif;
+    text-align: center;
+    color: rgb(204, 128, 29);
+    font-size: 30px;
+   margin-bottom: 30px;
+}
+.no .na{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+}
+.no .nmm{
+    color: #b0d722;
+    font-size: 18px;
+    font-style: normal;
+}
+.oo{
+    color:black;
+    font-size: 24px;
+    padding-left: 130px;
+    padding-top: 650px;
+}
+.o{
+    font-size: 24px;
+    color:black;
+    font-style: normal;
+    padding-left:130px;
+}
+.k{
+    font-size: 17px;
+    color:black;
+    font-style: normal;
+    padding-left:130px;
+
+}
